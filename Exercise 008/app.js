ad6fa24e148e16ee43d1ed02f026e7ec6cde9804
andy <mramjon@gmail.com>
2022-12-19 15:14:19 +0000

    Exercise 008 completed

---
+++ Exercise 008/app.js
@@ -0,0 +1,6 @@
+const testElement = document.getElementById("testButton");
+
+testElement.addEventListener("click", function() 
+{
+    document.getElementById("t1").innerHTML = "Test complete.";
+})
